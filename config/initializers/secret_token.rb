5c53454b5c78e2e2c35e006713432e762a0364fe
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-03 12:13:34 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wallstickers::Application.config.secret_token = '4f12713c4e63f752255054250dc04cfdc0d3bfb5ee987f1f1e54b71b7f45062f6384eac26128e6dab0eebfe051a1b9ddc6a390fc91755e71098fa0cf699aaa37'
