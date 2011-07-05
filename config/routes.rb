fcf36246850b635dcbccd78218a32e41d0fdae5f
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-05 10:42:06 +0200

    existing users can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Wallstickers::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root :to => "user_sessions#new"
+
+  get 'login' => 'user_sessions#new', :as => :login
+  get 'logout' => 'user_sessions#destroy', :as => :logout
+  post 'user_sessions' => 'user_sessions#create'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,7 +51,6 @@ Wallstickers::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe UserSessionsController do
+  it "should login username"
+
+  it "should login email"
+
+  it "should not login bad username"
+
+  it "should not login bad email"
+
+  it "should not login bad password"
+  
+  it "should logout user"
+end
