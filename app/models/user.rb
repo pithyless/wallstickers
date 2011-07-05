6e785888d4ae618bb494a004bc2a5f71b6177a59
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-05 09:11:25 +0200

    adds User#attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :password, :password_confirmation
+
   def self.find_by_username_or_email(login)
     return unless login
     if login.include? '@'
