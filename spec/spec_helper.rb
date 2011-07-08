930676be6d57ec34d1376b732649dacd90a4898c
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-08 15:49:44 +0200

    improves filepath locations of image uploads

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,8 +36,8 @@ Spork.prefork do
     config.after(:all) do
       # Get rid of uploaded images
       if Rails.env.test? || Rails.env.cucumber?
-        FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/tmp"])
-        FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/wallsticker"])
+        FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/test"])
+        FileUtils.rm_rf(Dir["#{Rails.root}/tmp/uploads/test"])
       end
     end
   end
