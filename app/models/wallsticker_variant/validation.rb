1b5c01bedad5567027dd5280e63a8e7268752715
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-11 18:11:45 +0200

    validates numericality of width, height, and price

--- app/models/wallsticker_variant/validation.rb
+++ app/models/wallsticker_variant/validation.rb
@@ -13,6 +13,10 @@ class WallstickerVariant < ActiveRecord::Base
   validates :height_cm,   :presence => true
   validates :price_pln,   :presence => true
 
+  validates_numericality_of :width_cm, :only_integer => true, :greater_than => 0
+  validates_numericality_of :height_cm, :only_integer => true, :greater_than => 0
+  validates_numericality_of :price_pln, :greater_than => 0
+
   def color=(value)
     write_attribute :color, value.try(:upcase)
   end
--- spec/models/wallsticker_variant_spec.rb
+++ spec/models/wallsticker_variant_spec.rb
@@ -7,6 +7,10 @@ describe WallstickerVariant, 'validates new' do
   it { should validate_presence_of :height_cm }
   it { should validate_presence_of :price_pln }
 
+  it { should validate_numericality_of :width_cm }
+  it { should validate_numericality_of :height_cm }
+  it { should validate_numericality_of :price_pln }
+
   it 'should fabricate' do
     sticker = Fabricate(:wallsticker_variant)
     sticker.should be_valid
