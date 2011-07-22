c080dd736ad60ab405966087dbb093757dfd4f8a
Michał Laskus <mlaskus@gmail.com>
2011-07-22 14:19:17 +0200

    Updated paths for wallsticker#show

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Wallstickers::Application.routes.draw do
 
   resources :decals, :as => 'wallstickers', :controller => 'wallstickers'
   get ':artist' => 'wallstickers#gallery', :as => 'artist_gallery'
-  get ':artist/:item' => 'wallstickers#show', :as => 'show_wallsticker'
+  get ':artist/:item' => 'wallstickers#show'
   post 'decals/:id/order' => 'wallstickers#add_to_cart', :as => 'add_wallsticker_to_cart'
 
   get 'cart' => 'shopping_cart#shopping_cart', :as => 'shopping_cart'
