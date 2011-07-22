a71e01a287e07034a06ce906a8c0f46e5ee6fdac
Michał Laskus <mlaskus@gmail.com>
2011-07-22 08:27:58 +0200

    Set the default_url_options to include :locale parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,4 +43,9 @@ class ApplicationController < ActionController::Base
     I18.locale = params[:locale] || I18.default_locale
   end
 
+  def default_url_options(options={})
+  logger.debug "default_url_options is passed options: #{options.inspect}\n"
+  { :locale => I18n.locale }
+end
+
 end
