d3a5539f0b05554e787fbf1e31e096db513030bd
Michał Laskus <mlaskus@gmail.com>
2011-07-22 08:13:51 +0200

    Added before_filter set_locale to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,9 +43,12 @@ class ApplicationController < ActionController::Base
     I18.locale = params[:locale] || I18.default_locale
   end
 
+<<<<<<< HEAD
   def default_url_options(options={})
   logger.debug "default_url_options is passed options: #{options.inspect}\n"
   { :locale => I18n.locale }
 end
 
+=======
+>>>>>>> Added before_filter set_locale to ApplicationController
 end
