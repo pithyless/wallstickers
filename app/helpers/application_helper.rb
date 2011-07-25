022faf30afef030f21f77bd36a5847b9088e928a
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-25 14:02:45 +0200

    fixes sorcery bug confusion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
   end
 
   def current_user_is?(user)
-    # TODO: https://github.com/NoamB/sorcery/issues/16
-    return false if current_user.nil? or current_user.nil?
+    return false unless current_user
     current_user.username == user.username
   end
 end
