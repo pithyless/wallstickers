4e25f1135ce6e91bf435826e67ca6521e9350111
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-25 12:26:41 +0200

    fixes check for current_user_is?(@artist)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def body_classes
     [controller.controller_name]
   end
+
+  def current_user_is?(user)
+    # TODO: https://github.com/NoamB/sorcery/issues/16
+    return false if current_user.nil? or current_user.nil?
+    current_user.username == user.username
+  end
 end
