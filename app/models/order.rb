f8641943aa567c4c57d73bb327dfc25f2d6246a1
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-07-25 19:21:46 +0200

    adds additional printing and shipping states to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,13 +15,29 @@ class Order < ActiveRecord::Base
     event :redirected_to_payment_gateway do
       transition :waiting_redirect_to_payment_gateway => :waiting_callback_from_payment_gateway
     end
+
+    event :verified_payment_gateway_transaction do
+      transition :waiting_callback_from_payment_gateway => :waiting_acceptance_by_printer
+    end
+
+    event :accepted_by_printer do
+      transition :waiting_acceptance_by_printer => :waiting_complete_printing
+    end
+
+    event :completed_printing do
+      transition :waiting_complete_printing => :waiting_shipping_package
+    end
+
+    event :shipped_package do
+      transition :waiting_shipping_package => :finished
+    end
   end
 
   state_machine :shipped_at, :initial => :pending, :namespace => 'shipment' do
     event :deliver do
       transition :pending => :shipped
     end
-    
+
     state :pending, :value => nil
     state :shipped, :if => lambda {|value| value}, :value => lambda {Time.now}
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,12 +14,95 @@ describe Order, 'validates brand new Order' do
   end
 end
 
-describe Order, 'initial states' do
+describe Order, 'stateful payment' do
   before :each do
     @order = Fabricate(:order)
   end
 
-  it 'should have initial states' do
-    @order.state_name.should          == :waiting_confirm_address_info
+  describe 'initial states' do
+    it 'should have initial states' do
+      @order.state_name.should  == :waiting_confirm_address_info
+      @order.paid_at.should be_nil
+      @order.printed_at.should be_nil
+      @order.shipped_at.should be_nil
+    end
+  end
+
+  describe 'confirming billing info' do
+
+    it 'should confirm address info' do
+      @order.state_name.should  == :waiting_confirm_address_info
+      @order.confirmed_address_info.should be_true
+      @order.state_name.should  == :waiting_redirect_to_payment_gateway
+    end
+
+    it 'should validate address info'
+  end
+
+  describe 'redirecting to payment gateway' do
+    it 'should redirect to gateway' do
+      @order.confirmed_address_info!
+      @order.redirected_to_payment_gateway.should be_true
+      @order.state_name.should  == :waiting_callback_from_payment_gateway
+    end
+  end
+
+  describe 'verifying payment gateway transaction' do
+    it 'should verify payment gateway transaction' do
+      @order.confirmed_address_info!
+      @order.redirected_to_payment_gateway!
+      @order.verified_payment_gateway_transaction.should be_true
+      @order.state_name.should  == :waiting_acceptance_by_printer
+    end
+
+    it 'should validate paid_at'
+  end
+end
+
+describe Order, 'stateful printing' do
+  before :each do
+    @order = Fabricate(:order)
+    @order.confirmed_address_info!
+    @order.redirected_to_payment_gateway!
+    @order.verified_payment_gateway_transaction!
+  end
+
+  describe 'waiting for acceptance by printer' do
+    it 'should be accepted by printer' do
+      @order.accepted_by_printer.should be_true
+      @order.state_name.should  == :waiting_complete_printing
+    end
+  end
+
+  describe 'waiting for printing completion' do
+    it 'should be printed' do
+      @order.accepted_by_printer!
+      @order.completed_printing.should be_true
+      @order.state_name.should  == :waiting_shipping_package
+    end
+
+    it 'should validate printed_at'
+  end
+end
+
+describe Order, 'stateful shipping' do
+  before :each do
+    @order = Fabricate(:order)
+    @order.confirmed_address_info!
+    @order.redirected_to_payment_gateway!
+    @order.verified_payment_gateway_transaction!
+    @order.accepted_by_printer!
+    @order.completed_printing!
+  end
+
+  describe 'waiting for shipping package' do
+    it 'should be shipped' do
+      @order.shipped_package.should be_true
+      @order.state_name.should  == :finished
+    end
+
+    it 'should validate shipped_at'
+
+    it 'should validate order is marked finished'
   end
 end
