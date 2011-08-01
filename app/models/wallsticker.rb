bfc9ffb72c448956ea276c89ea0e167433d388fa
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-01 17:27:54 +0200

    adds Wallsticker#image_urls and Wallsticker#image_url

--- app/models/wallsticker.rb
+++ app/models/wallsticker.rb
@@ -20,4 +20,13 @@ class Wallsticker < ActiveRecord::Base
   def self.top_featured(limit=5)
     self.order('created_at desc').limit(limit) # TODO
   end
+
+  def image_urls
+    # TODO: return PR images before source_image
+    [source_image_url]
+  end
+
+  def image_url
+    image_urls[0]
+  end
 end
