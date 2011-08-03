f65014170c8fbeb3ebde38bb65325dfc81fd0450
Michał Laskus <mlaskus@gmail.com>
2011-08-03 17:32:54 +0200

    Updates routing specs, corrects forms

--- spec/routing/users_routes_spec.rb
+++ spec/routing/users_routes_spec.rb
@@ -5,10 +5,14 @@ describe 'routes for Users' do
     user = Fabricate(:user, :username => 'jsmith')
   end
 
-  it 'routes /register to new user page' do
+  it 'routes get /register to new user page' do
     { :get => '/register' }.should route_to('users#new')
     { :get => new_user_path }.should route_to('users#new')
-    #TODO post routes
+  end
+
+  it 'routes post /register to create user page' do
+    { :post => '/register' }.should route_to('users#create')
+    { :post => create_user_path }.should route_to('users#create')
   end
 
   it 'routes /:username/profile to a users profile page' do
@@ -16,9 +20,13 @@ describe 'routes for Users' do
     { :get => profile_path(user) }.should route_to('users#profile', :user => 'jsmith')
   end
 
-  it 'routes /:username/profile/edit to a users edit profile page' do
+  it 'routes get /:username/profile/edit to a users edit profile page' do
     { :get => '/jsmith/profile/edit' }.should route_to('users#edit', :user => 'jsmith')
     { :get => edit_user_path(user) }.should route_to('users#edit', :user => 'jsmith')
-    #TODO put routes
+  end
+
+  it 'routes put /:username/profile/edit to an update profile page' do
+    { :put => '/jsmith/profile/edit' }.should route_to('users#update', :user => 'jsmith')
+    { :put => update_user_path(user) }.should route_to('users#update', :user => 'jsmith')
   end
 end
