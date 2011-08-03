2fa7a2b8065d1e5cbaa7966c151d1f14100bfb96
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-03 11:50:11 +0200

    useful rspec controller helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  def page
+    Capybara::Node::Simple.new(@response.body)
+  end
+
   it 'should find home' do
     get 'home'
     response.should be_success
+
+    page.should have_content('Inspirations')
   end
 end
