a08a7cc4f027143f4a4dd5b7106ef515b47823ad
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-08 13:27:01 +0200

    adds i18n translation fallback :pl => :en

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module Wallstickers
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :pl
+    config.i18n.fallbacks = {:pl => :en}
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
