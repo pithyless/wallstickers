6240c597effa29167c3ef303b5fff825f8dca949
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-10 21:01:12 +0200

    removes spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,34 +1,26 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'shoulda/matchers/integrations/rspec'
-  require 'cancan/matchers'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'shoulda/matchers/integrations/rspec'
+require 'cancan/matchers'
 
-  include Sorcery::TestHelpers::Rails
+include Sorcery::TestHelpers::Rails
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  RSpec.configure do |config|
-    config.mock_with :rspec
+RSpec.configure do |config|
+  config.mock_with :rspec
 
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
 
-    config.after(:all) do
-      # Get rid of uploaded images
-      if Rails.env.test? || Rails.env.cucumber?
-        FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/test"])
-        FileUtils.rm_rf(Dir["#{Rails.root}/tmp/uploads/test"])
-      end
+  config.after(:all) do
+    # Get rid of uploaded images
+    if Rails.env.test? || Rails.env.cucumber?
+      FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/test"])
+      FileUtils.rm_rf(Dir["#{Rails.root}/tmp/uploads/test"])
     end
   end
-end
-
-Spork.each_run do
-  # Wallstickers::Application.reload_routes!
-  load "Sporkfile.rb" if File.exists?("Sporkfile.rb")
 end
