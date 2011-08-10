77c1a7486fa6b607edca9137db36cd5b487a9965
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-10 15:42:41 +0200

    seeds Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# Categories
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+['For Him', 'For Her', 'Kitchen', 'Den', 'Bedroom'].each do |name|
+  c = Category.find_or_initialize_by_name(name)
+  c.save!
+end
