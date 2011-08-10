b0f1631ce9c1d58d4680c34a4f001d81be6d928e
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-10 15:42:24 +0200

    fixes slug_validator

--- lib/validators/slug_validator.rb
+++ lib/validators/slug_validator.rb
@@ -1,6 +1,6 @@
 class SlugValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, values)
-    unless value =~ /\A[0-9a-z_+-]\z/
+  def validate_each(record, attribute, value)
+    unless value =~ /\A[0-9a-z_+-]+\z/
       record.errors[attribute] << (options[:message] || "invalid slug: #{value}")
     end
   end
