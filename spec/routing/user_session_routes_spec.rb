da2c5f2fc0d0505e30c26bc2eb45e0cb6b609e3c
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-08-19 15:20:35 +0200

    artist edit profile link

--- spec/routing/user_session_routes_spec.rb
+++ spec/routing/user_session_routes_spec.rb
@@ -13,4 +13,10 @@ describe 'routes for UserSessions' do
     #       requires test to make sure it will always be 404
     # { :get  => '/user_sessions' }.should_not be_routable
   end
+
+  it 'should route artist profile edit' do
+    @artist = Fabricate :artist, :user => Fabricate(:user, :username => 'xyz')
+    { :get => '/profile/xyz/edit' }.should route_to('profiles#edit', :id => 'xyz')
+    { :get => edit_artist_profile_path(@artist) }.should route_to('profiles#edit', :id => 'xyz')
+  end
 end
