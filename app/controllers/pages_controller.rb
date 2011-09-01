a0c5ceae45ac2f9ccc9523aa6bd47b20b8d2f190
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-09-01 18:13:04 +0200

    promotes artist on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,6 @@ class PagesController < ApplicationController
     @categories = Category.all
     @active_category = Category.find_by_slug!('kitchen')
     @featured_wallstickers = Wallsticker.top_featured
+    @promoted_artist = Artist.find_by_username('krzysztof') # TODO
   end
 end
